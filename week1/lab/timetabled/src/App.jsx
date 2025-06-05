15ddd971fc573073db5b674a92d32e207fed4367
Justin Chen <justinyijiachen@gmail.com>
2025-06-04 21:07:22 -0400

    week 1

---
+++ week1/lab/timetabled/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calendar from './components/Calendar'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>My Title</h1>
+      <h2>My Subtitle</h2>
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
