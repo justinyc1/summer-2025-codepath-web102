bf063417ede78cdf3680b8f6e611ec87742eb054
justinyc1 <justinyijiachen@gmail.com>
2025-07-15 02:19:30 -0400

    week6 project basic fetch

--- week6/project/Data Dashboard/src/App.jsx
+++ week6/project/Data Dashboard/src/App.jsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+// at least 10 unique items, one per row
+// at least 2 features per row
+// 3 summaries about data
+// useEffect, async await
+// search bar to filter item, dynamically
+// multiple simulaneous filters
+// filter different input types (text, dropdown, buttons, etc)
+// user can enter specific bounds for filter values
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
+
+  const fetchData = async (section) => {
+    try {
+      const response = await fetch(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${API_KEY}`);
+      const jsonData = await response.json();
+      setData(jsonData);
+      console.log(jsonData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData("world");
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className='header'>
+        <h1>News</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='content'>
+        {data !== null && "Section: " + data.section}
+        {data !== null && data.results.map((result) => {
+          return (
+            <p>{result.title}</p>
+          )
+        })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
