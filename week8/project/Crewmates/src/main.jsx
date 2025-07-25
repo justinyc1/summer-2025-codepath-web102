b4949d91c1171aaf19dc7944c5efe6913a826bd7
Justin <justinyijiachen@gmail.com>
2025-07-25 18:55:26 -0400

    week8 project create page

--- week8/project/Crewmates/src/main.jsx
+++ week8/project/Crewmates/src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Home from './pages/Home.jsx'
+import CreateCrewmate from './pages/CreateCrewmate.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/create" element={<CreateCrewmate />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
--- week8/project/Crewmates/src/pages/CreateCrewmate.jsx
+++ week8/project/Crewmates/src/pages/CreateCrewmate.jsx
@@ -1,1 +1,62 @@
+import { useState } from 'react'
+import './CreateCrewmate.css'
 // user can: name, select an attribute
+
+function CreateCrewmate() {
+    const [selections, setSelections] = new useState({
+        name: "",
+        sus: false,
+        speed: "",
+        color: ""
+    });
+    const colors = ["red", "orange", "yellow", "lime", "green", "cyan", "blue", "purple", "pink", "brown", "white", "black"]
+
+    const updateSelection = (event, attribute) => {
+        switch (attribute) {
+            case "name":
+                setSelections({...prevSelections, [attribute]: event.target.value})
+                break
+            case "sus":
+
+                break
+            case "speed":
+
+                break
+            case "color":
+
+                break
+        }
+    }
+
+    return (
+        <div className='create-container'>
+            <h1>Create a New Crewmate</h1>
+            <div className='selections-container'>
+                <div className='name-container'>
+                    Name:
+                    <input 
+                        type="text"
+                        value={selections.name}
+                        onChange={(event) => {updateSelection(event, "name")}}
+                    />
+                </div>
+                <div className='sus-container'>
+
+                </div>
+                <div className='speed-container'>
+                    Speed {"(mph)"}:
+                    <input 
+                        type="text"
+                        value={selections.speed}
+                        onChange={(event) => {updateSelection(event, "speed")}}
+                    />
+                </div>
+                <div className='color-container'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CreateCrewmate
