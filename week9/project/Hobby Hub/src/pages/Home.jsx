32997442111f3883baa1cbe43d58179361119f20
Justin Chen <justinyijiachen@gmail.com>
2025-07-31 18:48:43 -0400

    week9 project init

---
+++ week9/project/Hobby Hub/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.css'
+
+// map website, can create list of things to go, can share with friends, can display on a map
+
+function Home() {
+
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
+
+export default Home
